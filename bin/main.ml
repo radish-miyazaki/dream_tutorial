2f0b5ca95b7b9eb61579c49ad1a7903afcd70a11
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-07-13 15:43:52 +0900

    add custom middleware (before process request)

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,14 @@
+let count = ref 0
+
+let count_requests inner_handler request =
+  count := !count + 1;
+  Printf.printf "Request #%d\n%!" !count;
+  inner_handler request
+
 let () =
   Dream.run
   @@ Dream.logger
+  @@ count_requests
   @@ Dream.router [
     Dream.get "/"
       (fun _ -> Dream.html "Good morning, world!");
